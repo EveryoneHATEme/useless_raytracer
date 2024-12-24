6e3e52535ad520c5cfdb7fbde63bd6927e4ee3c7
EveryoneHATEme <peppathepiginrussia@gmail.com>
2024-12-24 19:56:26 +0300

    fixed program close

--- main.cpp
+++ main.cpp
@@ -43,38 +43,40 @@ int main()
         return -1;
     }
 
-    Scene scene;
-    scene.addMaterial({ glm::vec3(0.6f, 0.6f, 0.6f), 0.5f, glm::vec3(0.7f, 0.05f, 0.7f), 2.f }); // ground
-    scene.addMaterial({ glm::vec3(1.0f, 0.2f, 0.2f), 0.3f, glm::vec3(0.f), 0.f }); // diffuse 
-    scene.addMaterial({ glm::vec3(0.2f, 0.2f, 1.0f), 1.0f, glm::vec3(0.f), 0.f }); // metal 
-    scene.addMaterial({ glm::vec3(1.f, 1.f, 1.f), 0.f, glm::vec3(0.1f, 0.1f, 1.f), 5.f }); // white light
-    scene.addMaterial({ glm::vec3(1.f, 0.6f, 0.f), 0.f, glm::vec3(1.f, 0.6f, 0.01f), 10.f }); // yellow light
-
-    scene.addSphere({ glm::vec3(0.0f, 100.5f, 1.0f), 100.f, 0, 0.0, 0.0, 0.0 }); // ground
-    scene.addSphere({ glm::vec3(2.0f, 0.0f, 0.0f), 0.5f, 1, 0.0, 0.0, 0.0 }); // diffuse
-    scene.addSphere({ glm::vec3(-2.0f, 0.0f, 0.0f), 0.5f, 2, 0.0, 0.0, 0.0 }); // metal
-    scene.addSphere({ glm::vec3(0.0f, -2.0f, 3.0f), 0.5f, 3, 0.0, 0.0, 0.0 }); // white light
-    scene.addSphere({ glm::vec3(0.0f, 0.0f, -3.0f), 0.5f, 4, 0.0, 0.0, 0.0 }); // yellow light
-
-    Camera camera(windowWidth, windowHeight, glm::vec3(0.0f, 0.0f, -2.0f), glm::vec3(0.0f, 0.0f, 1.0f), glm::radians(45.f), 0.1f, 100.f);
-
-    Renderer renderer(camera, scene);
-
-    lastFrameTime = (float)glfwGetTime();
-
-    while (!glfwWindowShouldClose(window))
     {
-        float currentFrameTime = (float)glfwGetTime();
-        float deltaTime = currentFrameTime - lastFrameTime;
-        lastFrameTime = currentFrameTime;
+        Scene scene;
+        scene.addMaterial({ glm::vec3(0.6f, 0.6f, 0.6f), 0.5f, glm::vec3(0.7f, 0.05f, 0.7f), 2.f }); // ground
+        scene.addMaterial({ glm::vec3(1.0f, 0.2f, 0.2f), 0.3f, glm::vec3(0.f), 0.f }); // diffuse 
+        scene.addMaterial({ glm::vec3(0.2f, 0.2f, 1.0f), 1.0f, glm::vec3(0.f), 0.f }); // metal 
+        scene.addMaterial({ glm::vec3(1.f, 1.f, 1.f), 0.f, glm::vec3(0.1f, 0.1f, 1.f), 5.f }); // white light
+        scene.addMaterial({ glm::vec3(1.f, 0.6f, 0.f), 0.f, glm::vec3(1.f, 0.6f, 0.01f), 10.f }); // yellow light
 
-        camera.update(window, deltaTime);
-        renderer.draw(window);
+        scene.addSphere({ glm::vec3(0.0f, 100.5f, 1.0f), 100.f, 0, 0.0, 0.0, 0.0 }); // ground
+        scene.addSphere({ glm::vec3(2.0f, 0.0f, 0.0f), 0.5f, 1, 0.0, 0.0, 0.0 }); // diffuse
+        scene.addSphere({ glm::vec3(-2.0f, 0.0f, 0.0f), 0.5f, 2, 0.0, 0.0, 0.0 }); // metal
+        scene.addSphere({ glm::vec3(0.0f, -2.0f, 3.0f), 0.5f, 3, 0.0, 0.0, 0.0 }); // white light
+        scene.addSphere({ glm::vec3(0.0f, 0.0f, -3.0f), 0.5f, 4, 0.0, 0.0, 0.0 }); // yellow light
 
-        glfwSwapBuffers(window);
-        glfwPollEvents();
+        Camera camera(windowWidth, windowHeight, glm::vec3(0.0f, 0.0f, -2.0f), glm::vec3(0.0f, 0.0f, 1.0f), glm::radians(45.f), 0.1f, 100.f);
+
+        Renderer renderer(camera, scene);
+
+        lastFrameTime = (float)glfwGetTime();
+
+        while (!glfwWindowShouldClose(window))
+        {
+            float currentFrameTime = (float)glfwGetTime();
+            float deltaTime = currentFrameTime - lastFrameTime;
+            lastFrameTime = currentFrameTime;
+
+            camera.update(window, deltaTime);
+            renderer.draw(window);
+
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+        }
     }
-
+    
     glfwTerminate();
 
     return 0;
